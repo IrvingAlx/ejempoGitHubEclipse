786d419d7dacf7d033925b26624fd8ff1ca83caf
jhons <jhons@LAPTOP-D275LOND>
2022-02-03 16:56:59 -0600

    Ejemplo 2

--- src/tiposPerro/tiposPerro.java
+++ src/tiposPerro/tiposPerro.java
@@ -6,7 +6,7 @@ public class tiposPerro {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		pastorAleman dog1 = new pastorAleman("Grande","Largo","35kg");
-		chihuahua dog2 = new chihuahua("Chico","Corto","10kg");
+		chihuahua dog2 = new chihuahua("Chico","Cortooo","10kg");
 		
 		dog1.saludar();
 		dog1.truco();
